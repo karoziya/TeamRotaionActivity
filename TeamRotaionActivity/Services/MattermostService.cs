11d3ece2f21c73d207c55d330605b4c4c6ac8b3d
Камышев Сергей (Kamyshev_SN) <Kamyshev_SN@directum.ru>
2023-11-19 21:21:35 +0400

    Сервис отправки сообщений

---
+++ TeamRotaionActivity/Services/MattermostService.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Net.Http.Headers;
+
+namespace TeamRotationActivity.Services
+{
+    public class MattermostService
+    {
+        public static void SendMessage(string message)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "sztoyqk4a3gk3cf1monfxhodgo");
+                var uri = "https://talk.directum.ru/api/v4/posts";
+                var body = new
+                {
+                    channel_id = "ascufsthxibpiq9x7bnqbycimc",
+                    message = message
+                };
+                var result = client.PostAsJsonAsync(uri, body).Result;                
+            }
+        }
+    }
+}
