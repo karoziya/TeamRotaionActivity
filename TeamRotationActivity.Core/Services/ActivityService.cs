82651218502ebcd66efe74c0a47454f695b107e0
m.minaev <mminmax@gmail.com>
2023-11-20 23:57:21 +0300

    Почистил немного. Сверстал страницу добавления Member и Activity.

--- TeamRotationActivity.Core/Services/ActivityService.cs
+++ TeamRotationActivity.Core/Services/ActivityService.cs
@@ -1,8 +1,26 @@
 ﻿using TeamRotationActivity.Domain.Interfaces.Services;
+using TeamRotationActivity.Domain.Models;
 
 namespace TeamRotationActivity.Core.Services;
 
 public class ActivityService : IActivityService
 {
+    private List<ActivityWork> _activities = new List<ActivityWork>();
+
+    public ActivityWork Create(ActivityWork entity)
+    {
+        _activities.Add(entity);
+        return entity;
+    }
+
+    public List<ActivityWork> GetAll()
+    {
+        return _activities;
+    }
+
+    public ActivityWork? GetById(Guid id)
+    {
+        return _activities.FirstOrDefault(m => m.Id == id);
+    }
 }
 
---
+++ TeamRotationActivity.Core/Services/MemberService.cs
@@ -0,0 +1,26 @@
+﻿using TeamRotationActivity.Domain.Interfaces.Services;
+using TeamRotationActivity.Domain.Models;
+
+namespace TeamRotationActivity.Core.Services;
+
+public class MemberService : IMemberService
+{
+    private List<Member> _members = new List<Member>();
+
+    public Member Create(Member entity)
+    {
+        _members.Add(entity);
+        return entity;
+    }
+
+    public List<Member> GetAll()
+    {
+        return _members;
+    }
+
+    public Member? GetById(Guid id)
+    {
+        return _members.FirstOrDefault(m => m.Id == id);
+    }
+}
+
